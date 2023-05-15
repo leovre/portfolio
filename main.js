51a79e228ab5bce328519df35da982c3e9fb3b34
Leo <leonardovrodriguez246@gmail.com>
2023-05-14 23:09:16 -0400

    added overlays to project images

---
+++ main.js
@@ -0,0 +1,15 @@
+// const imageContainers = document.querySelectorAll('.image-container');
+
+// imageContainers.forEach(container => {
+//     const img = container.querySelector('img');
+//     const overlay = container.querySelector('.overlay');
+//     const projectName = 'SHRED'; // Replace with actual project name
+//     const githubLink = 'https://github.com/your-username/your-repo'; // Replace with actual GitHub link
+//     const deployedSiteLink = 'https://your-deployed-site.com'; // Replace with actual deployed site link
+
+//     overlay.innerHTML = `
+//     <h3>${projectName}</h3>
+//     <a href="${githubLink}" target="_blank">GitHub</a>
+//     <a href="${deployedSiteLink}" target="_blank">Deployed Site</a>
+//   `;
+// });
